3c830360f974a3d9ddc1a47c630326f5fbe939fa
Flamesone2 <sergbasov.com@gmail.com>
2025-09-27 14:55:13 +0300

    add views for rooms and room reservations

--- dj_drf_hotel_proj/hotels/views.py
+++ dj_drf_hotel_proj/hotels/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Room, RoomReservation
+from .serializers import RoomSerializer, RoomReservationSerializer
 # Create your views here.
+class RoomsAPIViev(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class RoomReservationsAPIViev(generics.ListAPIView):
+    queryset = RoomReservation.objects.all()
+    serializer_class = RoomReservationSerializer
