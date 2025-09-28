926053355918ba48c745511e0aadc24e30c87d7f
Flamesone2 <sergbasov.com@gmail.com>
2025-09-28 17:08:53 +0300

    add data validation to RoomReservationSerializer

--- dj_drf_hotel_proj/hotels/serializers.py
+++ dj_drf_hotel_proj/hotels/serializers.py
@@ -6,11 +6,17 @@ from .models import RoomReservation, Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ("room_id", "room_number", "description",
-                  "price_for_a_night")
-
+        fields = '__all__'
+# serializers.py
 class RoomReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomReservation
-        fields = ("booking_id", "room_id", "date_start",
-                  "date_end")
+        fields = ['booking_id', 'room', 'date_start', 'date_end']
+    def validate(self, data):
+        date_start = data.get('date_start')
+        date_end = data.get('date_end')
+        if date_start and date_end and date_end < date_start:
+            raise serializers.ValidationError(
+                "Дата окончания бронирования не может быть раньше даты начала."
+            )
+        return data
