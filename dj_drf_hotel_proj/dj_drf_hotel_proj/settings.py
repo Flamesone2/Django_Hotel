d532cbb5b77a6f2018921c1cd4bd9d41dbddd3a0
Flamesone2 <sergbasov.com@gmail.com>
2025-09-30 10:36:11 +0300

    implementing decouple for storing .env variables

--- dj_drf_hotel_proj/dj_drf_hotel_proj/settings.py
+++ dj_drf_hotel_proj/dj_drf_hotel_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sa#yymij731&cayb2%i=-zlg=pifbf-5=f+j^tb5)k9nv!vt2c'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
